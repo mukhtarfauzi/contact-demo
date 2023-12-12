ad892a129805f3418675205bf8f13482ea4d2f22
mukhtarfauzi <mukhtar.fz@gmail.com>
2023-12-12 09:16:16 +0700

    test: try fake api

---
+++ test/unit_test.dart
@@ -0,0 +1,10 @@
+import 'package:contact_demo/data/services/api_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('get Contact', () async {
+    final repository = AppRepository();
+    final contactList = await repository.restClient.getContact();
+    expect(contactList.contacts?.length, (int length) => length > 0);
+  });
+}
